93705c40ab01ede1908330d651e73ab717ba01fc
milos <md20173715@student.fon.bg.ac.rs>
2020-03-12 21:40:52 +0100

    my blog full stack app

---
+++ build/precache-manifest.46a7ca40107b453131f5e3967e10d16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363c5f9c2df69a7b056273582d1e20b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26930b12a4f6ad738926",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "3c2d85dc8cb429e84246",
+    "url": "/static/js/2.cbb2a636.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cbb2a636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26930b12a4f6ad738926",
+    "url": "/static/js/main.6e4289fc.chunk.js"
+  },
+  {
+    "revision": "f8c88a4ab800e2a20047",
+    "url": "/static/js/runtime-main.ae3b9067.js"
+  }
+]);
